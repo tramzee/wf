97609fa33cdbc6f4e084243d6a565dc1073ee0c1
Tim Ramsey <tramsey@gmail.com>
2022-04-15 22:20:14 +0000

    Fix bug using a function as a field.

--- compose.go
+++ compose.go
@@ -300,7 +300,7 @@ func toValue0(a *arena, v interface{}, ftype reflect.Type) (typ dataType, val ui
 			if !m.Valid() {
 				return 0, 0, fmt.Errorf("invalid IPRange %v", m)
 			}
-			r, err := toRange0(a, Range{m.From, m.To}, ftype)
+			r, err := toRange0(a, Range{m.From(), m.To()}, ftype)
 			if err != nil {
 				return 0, 0, err
 			}
